7d5e69295a83ffa010461100bdf76db4dbec5bf5
abhlab2024 <abh26417@gmail.com>
2025-07-15 12:46:14 +0530

    testing connection for own

--- php_server/index.php
+++ php_server/index.php
@@ -5,4 +5,26 @@ echo "Hello, World! This is a PHP server response.";
 phpinfo();
 
 
+
+
+$servername = "db3";
+$username = "root";
+$password = "g7@Vx#2q";
+$dbname = "testdb"; // utf8mb4_general_ci   
+
+
+
+// pdo connection
+try {
+    $connpdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $connpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+
+
+
 ?>
