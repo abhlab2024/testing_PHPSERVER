84176cfdde197787b5a35bdcc7ffca6a286b1bdd
abhlab2024 <abh26417@gmail.com>
2025-07-15 13:52:23 +0530

    testing

--- php_server/index.php
+++ php_server/index.php
@@ -22,7 +22,7 @@ try {
     echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-    die();
+    // die();
 }
 
 
@@ -44,7 +44,31 @@ try {
     echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-    die();
+    // die();
+}
+
+
+
+
+echo '----------------------------------------------------------------------------------------------------------------';
+
+
+$servername = "db3";
+$username = "root";
+$password = "g7@Vx#2q";
+$dbname = "testdb"; // utf8mb4_general_ci   
+
+
+
+// pdo connection
+try {
+    $connpdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $connpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    // die();
 }
 
 
